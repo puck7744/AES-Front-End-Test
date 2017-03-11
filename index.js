de6f03a3660cce61609c5c11a65eafc8452ae3f6
Tristan Shelton <puck7744@gmail.com>
2017-03-11 11:55:37 -0500

    Better code structure

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ var express = require('express');
 var app = express();
 require('./src/render.js')(app);
 
+var QuestionSet = require('./src/QuestionSet.js')
+
 // Set up Environment
 app.set('port', (process.env.PORT || 5000));
 app.set('views', 'src/views');
 app.set('view engine', 'jsx');
 
+// This route amounts to our minimal controller
 app.get('/', function (req, res) {
+  res.locals.questions = new QuestionSet().narrow(10).noanswers.array;
+
   res.render('index');
 });
 
--- src/views/index.jsx
+++ src/views/index.jsx
@@ -1,18 +1,16 @@
 var React = require('react');
 
-var QuestionSet = require('../QuestionSet.js')
-
-let questions = new QuestionSet().narrow(10).noanswers.array;
-
-module.exports = (
-  <html>
-    <head>
-      <title>Sample Quiz</title>
-    </head>
-    <body>
-      <div id="container" />
-      <script id="initial-data" data-json={JSON.stringify(questions)} />
-      <script src="/bundle.js" />
-    </body>
-  </html>
-)
+module.exports = function(options) {
+  return (
+    <html>
+      <head>
+        <title>Sample Quiz</title>
+      </head>
+      <body>
+        <div id="container" />
+        <script id="initial-data" data-json={JSON.stringify(options._locals.questions)} />
+        <script src="/bundle.js" />
+      </body>
+    </html>
+  )
+}
