185f4a74264f96ade524a2c4fa708513af09a7c5
Tristan Shelton <puck7744@gmail.com>
2017-03-11 17:48:57 -0500

    Update tests

--- test/model.js
+++ test/model.js
@@ -1,7 +1,7 @@
 var assert = require('assert');
 
 describe('Quiz model', function () {
-  let QuestionSet = require('../src/QuestionSet');
+  let QuestionSet = require('../src/model/Quiz.js');
   let questions;
 
   beforeEach(function () {
@@ -10,8 +10,16 @@ describe('Quiz model', function () {
 
   it('can be chained', function() {
     assert.equal(questions, questions.select());
+    assert.equal(questions, questions.clone);
     assert.equal(questions, questions.noanswers);
   });
+
+  it('can accept an input array', function() {
+    questions = new QuestionSet([{answer: 'A'}]);
+
+    assert.equal(questions.array[0].answer, 'A');
+  });
+
   it('#array returns valid question objects', function() {
     questions.array.forEach((question) => {
       assert.notEqual(question.text, undefined);
@@ -19,6 +27,7 @@ describe('Quiz model', function () {
       assert.notEqual(question.choice_a, undefined);
     });
   });
+
   it('#select(1000).array returns all questions', function() {
     let length = questions.array.length;
 
@@ -38,6 +47,18 @@ describe('Quiz model', function () {
     assert.equal(result.constructor, Array);
     assert.equal(result.length, 0);
   });
+
+  it('#clone returns new objects', function() {
+    let first = questions.array;
+    let second = questions.clone.array;
+
+    second[0].changed = true;
+    second[1] = null;
+
+    assert.notEqual(first[0].changed, true);
+    assert.notEqual(first[1], null);
+  });
+
   it('#noanswers leaves no answer properties', function() {
     let result = questions.noanswers.array;
 
--- test/server.js
+++ test/server.js
@@ -1,6 +1,6 @@
 var request = require('supertest');
 
-describe('loading express', function () {
+describe('Express server', function () {
   let server;
 
   beforeEach(function () {
@@ -12,22 +12,35 @@ describe('loading express', function () {
     server.close(done);
   });
 
-  it('responds to /', function(done) {
+  it('redirects from /', function(done) {
     request(server)
       .get('/')
+      .expect(301, done);
+  });
+
+  it('responds to /quiz/', function(done) {
+    request(server)
+      .get('/quiz/')
+      .expect(200, done);
+  });
+
+  it('can post answers to /quiz/submit/', function(done) {
+    request(server)
+      .post('/quiz/submit/')
+      .field('answers', Array(10).fill('a'))
       .expect(200, done);
   });
 
   it('serves Javascript at /bundle.js', function(done) {
     this.timeout(10000); // Bundling may take some time
-    
+
     request(server)
       .get('/bundle.js')
       .expect(200)
       .expect('Content-Type', 'application/javascript', done);
   });
 
-  it('404 non-existent page', function(done) {
+  it('404s non-existent page', function(done) {
     request(server)
       .get('/foobar')
       .expect(404, done);
