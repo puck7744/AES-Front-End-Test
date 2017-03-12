45d4c131f7c1a2c4151eb2f42415cfd59f37bde8
Tristan Shelton <puck7744@gmail.com>
2017-03-12 02:12:24 -0400

    Front end grading

--- src/view/components/Grade.jsx
+++ src/view/components/Grade.jsx
@@ -13,17 +13,63 @@ class Grade extends React.Component {
   }
 
   render() {
+    let content = null;
+
     if (this.state.finished) {
       if (this.state.success) {
-        return <h1>All done!</h1>;
+        content = [
+          <h1>Your Grade: {this.grade}</h1>,
+          <p>You answered {this.correct} out of {this.total} correctly ({Math.round((this.correct/this.total)*100)}%)</p>
+        ];
       }
       else {
-        return <h1>Something went wrong!</h1>;
+        content = <h1>Something went wrong!</h1>;
       }
     }
     else {
-      return <h1>Your answers are being checked...</h1>;
+      content = <h1>Your answers are being checked...</h1>;
     }
+
+    return (
+      <div className="grade">
+        {content}
+      </div>
+    );
+  }
+
+  get correct() {
+    if (!this.state.results || this.state.results.constructor !== Array)
+      return 0;
+
+    return this.state.results.filter(function(n){return n}).length;
+  }
+
+  get total() {
+    if (!this.state.results || this.state.results.constructor !== Array)
+      return 0;
+
+    return this.state.results.length;
+  }
+
+  get grade() {
+    // http://www.cs.uni.edu/~mccormic/lettergrade.html
+    if (this.state.results && this.state.results.constructor === Array) {
+      let percent = Math.round((this.correct/this.total)*100);
+
+      if (percent > 93) return 'A';
+      if (percent > 89) return 'A-';
+      if (percent > 86) return 'B+';
+      if (percent > 82) return 'B';
+      if (percent > 79) return 'B-';
+      if (percent > 76) return 'C+';
+      if (percent > 72) return 'C';
+      if (percent > 69) return 'C-';
+      if (percent > 66) return 'D+';
+      if (percent > 62) return 'D';
+      if (percent > 60) return 'D-';
+      return 'F';
+    }
+    return '?';
   }
 
   submit() {
@@ -35,11 +81,13 @@ class Grade extends React.Component {
     request.onload = function() {
       switch (request.status) {
         case 200:
-          scope.setState({
-            finished: true,
-            success: true
-          });
-          console.log(request.response);
+          scope.setState(Object.assign(
+            {
+              finished: true,
+              success: true
+            },
+            JSON.parse(request.response)
+          ));
           break;
         default:
           scope.setState({
